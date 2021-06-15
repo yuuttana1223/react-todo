5cb3a5b7eb125dd71b0530f3bd2a6e3c8d88159b
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-06-16 00:40:58 +0900

    [Finish] セクション53までのCSS・タスク追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,60 @@
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./styles.css";
 
 export const App = () => {
-  return <div></div>
+  const [todoText, setTodoText] = useState("");
+  const [incompleteTodos, setIncompleteTodos] = useState(["あああ", "いいい"]);
+  const [completeTodos, setCompleteTodos] = useState(["ううう", "えええ"]);
+
+  const onChangeTodoText = (e) => setTodoText(e.target.value);
+
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
+
+  return (
+    <>
+      <div className="input-area">
+        {/* onChangeは入力が変わるたび */}
+        <input
+          placeholder="TODOを入力"
+          type="text"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          {incompleteTodos.map((todo) => {
+            return (
+              // keyは何個目の要素かを覚えておくための目印
+              <li key={todo}>
+                <span>{todo}</span>
+                <button>完了</button>
+                <button>削除</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
+        <ul>
+          {completeTodos.map((todo) => {
+            return (
+              <li key={todo}>
+                <span>{todo}</span>
+                <button>戻す</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
 };
